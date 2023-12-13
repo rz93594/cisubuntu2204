aa0672769b52099e9e05413198e7a15d462d5a46
Todd M. Wilkinson <todd.wilkinson@elanco.com>
2023-12-13 10:06:15 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,11 +7,13 @@ if ! command -v git &> /dev/null
 then
     echo "git could not be found"
     sudo apt update -y && sudo apt install git -y
+    cd /var/tmp
     git clone https://github.com/rz93594/cisubuntu2204.git
     echo "Git downloaded"
     cd cisubuntu2204
     source ./cis-lbk_ubuntu_22.04_LTS-v1.0.0.sh -y
 else
+    cd /var/tmp
     git clone https://github.com/rz93594/cisubuntu2204.git
     echo "Git downloaded"
     cd cisubuntu2204
